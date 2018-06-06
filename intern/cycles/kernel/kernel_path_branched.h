496320848e2ec0d2c7b89b5205407ddc2cc65356
Stefan Werner <stefan.werner@tangent-animation.com>
2018-06-06 14:53:13 +0200

    Cycles: Another fix for deferred ray marching, this time a buffer underflow.

--- intern/cycles/kernel/kernel_path_branched.h
+++ intern/cycles/kernel/kernel_path_branched.h
@@ -524,7 +524,9 @@ ccl_device void kernel_branched_path_integrate(KernelGlobals *kg,
           } while (state.volume_stack[j].shader != SHADER_NONE);
           --i;
         }
-        state.volume_stack[i].t_enter = 0.0f;
+        if (i > 0) {
+          state.volume_stack[i].t_enter = 0.0f;
+        }
       }
       volume_ray.P = save_p;
     }

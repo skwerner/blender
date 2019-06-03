eliemichel/OpenMfxForBlender

Automatically set frame range
Importer gets frame range from the available files in the sequence.

source/blender/editors/io/io_openvdb.c
@@ -9,17 +9,21 @@
 
 #include "DNA_mesh_types.h"
 #include "DNA_modifier_types.h"
+#include "DNA_object_force.h"
 #include "DNA_object_types.h"
 #include "DNA_scene_types.h"
+#include "DNA_smoke_types.h"
 #include "DNA_space_types.h"
 
 #include "BKE_context.h"
+#include "BKE_global.h"
 #include "BKE_main.h"
 #include "BKE_mesh.h"
 #include "BKE_modifier.h"
 #include "BKE_object.h"
 #include "BKE_report.h"
 
+#include "BLI_fileops.h"
 #include "BLI_listbase.h"
 #include "BLI_path_util.h"
 #include "BLI_string.h"
@@ -32,6 +36,37 @@
 #include "io_openvdb.h"
 
 #include "openvdb_capi.h"
+
+static void get_frame_range(Object *ob, char *path, int *r_start, int *r_end)
+{
+	char filepath[FILE_MAX];
+	char head[FILE_MAX], tail[FILE_MAX];
+	unsigned short numlen;
+
+	BLI_strncpy(filepath, path, sizeof(filepath));
+
+	if (BLI_path_is_rel(filepath)) {
+		BLI_path_abs(filepath, ID_BLEND_PATH(G.main, (ID *)ob));
+	}
+
+	*r_start = *r_end = BLI_stringdec(filepath, head, tail, &numlen);
+
+	/* Lower bound. */
+	BLI_stringenc(filepath, head, tail, numlen, (*r_start - 1));
+
+	while (*r_start > 0 && BLI_exists(filepath)) {
+		(*r_start)--;
+		BLI_stringenc(filepath, head, tail, numlen, (*r_start - 1));
+	}
+
+	/* Upper bound. */
+	BLI_stringenc(filepath, head, tail, numlen, (*r_end + 1));
+
+	while (BLI_exists(filepath)) {
+		(*r_end)++;
+		BLI_stringenc(filepath, head, tail, numlen, (*r_end + 1));
+	}
+}
 
 static void wm_openvdb_import_draw(bContext *UNUSED(C), wmOperator *op)
 {
@@ -50,6 +85,7 @@ static int wm_openvdb_import_exec(bContext *C, wmOperator *op)
 
 	Main *bmain = CTX_data_main(C);
 	Scene *scene = CTX_data_scene(C);
+	PointCache *cache;
 	char filepath[FILE_MAX];
 	char filename[64];
 	char cachename[64];
@@ -66,7 +102,15 @@ static int wm_openvdb_import_exec(bContext *C, wmOperator *op)
 	OpenVDBModifierData *vdbmd = (OpenVDBModifierData *)md;
 	BLI_addtail(&ob->modifiers, md);
 
+	cache = vdbmd->smoke->domain->point_cache[0];
+
 	BLI_strncpy(vdbmd->filepath, filepath, 1024);
+
+	get_frame_range(ob, vdbmd->filepath, &cache->startframe, &cache->endframe);
+
+	vdbmd->frame_offset = cache->startframe - 1;
+	cache->endframe -= cache->startframe - 1;
+	cache->startframe = 1;
 
 	return OPERATOR_FINISHED;
 }

skwerner/blender

source/blender/editors/io/io_openvdb.c
@@ -9,17 +9,21 @@
 
 #include "DNA_mesh_types.h"
 #include "DNA_modifier_types.h"
+#include "DNA_object_force_types.h"
 #include "DNA_object_types.h"
 #include "DNA_scene_types.h"
+#include "DNA_smoke_types.h"
 #include "DNA_space_types.h"
 
 #include "BKE_context.h"
+#include "BKE_global.h"
 #include "BKE_main.h"
 #include "BKE_mesh.h"
 #include "BKE_modifier.h"
 #include "BKE_object.h"
 #include "BKE_report.h"
 
+#include "BLI_fileops.h"
 #include "BLI_listbase.h"
 #include "BLI_path_util.h"
 #include "BLI_string.h"
@@ -32,6 +36,37 @@
 #include "io_openvdb.h"
 
 #include "openvdb_capi.h"
+
+static void get_frame_range(Object *ob, char *path, int *r_start, int *r_end)
+{
+	char filepath[FILE_MAX];
+	char head[FILE_MAX], tail[FILE_MAX];
+	unsigned short numlen;
+
+	BLI_strncpy(filepath, path, sizeof(filepath));
+
+	if (BLI_path_is_rel(filepath)) {
+		BLI_path_abs(filepath, ID_BLEND_PATH(G.main, (ID *)ob));
+	}
+
+	*r_start = *r_end = BLI_stringdec(filepath, head, tail, &numlen);
+
+	/* Lower bound. */
+	BLI_stringenc(filepath, head, tail, numlen, (*r_start - 1));
+
+	while (*r_start > 0 && BLI_exists(filepath)) {
+		(*r_start)--;
+		BLI_stringenc(filepath, head, tail, numlen, (*r_start - 1));
+	}
+
+	/* Upper bound. */
+	BLI_stringenc(filepath, head, tail, numlen, (*r_end + 1));
+
+	while (BLI_exists(filepath)) {
+		(*r_end)++;
+		BLI_stringenc(filepath, head, tail, numlen, (*r_end + 1));
+	}
+}
 
 static void wm_openvdb_import_draw(bContext *UNUSED(C), wmOperator *op)
 {
@@ -51,6 +86,7 @@ static int wm_openvdb_import_exec(bContext *C, wmOperator *op)
 	Main *bmain = CTX_data_main(C);
 	Scene *scene = CTX_data_scene(C);
     ViewLayer *viewlayer = CTX_data_view_layer(C);
+	PointCache *cache;
 	char filepath[FILE_MAX];
 	char filename[64];
 	char cachename[64];
@@ -67,7 +103,15 @@ static int wm_openvdb_import_exec(bContext *C, wmOperator *op)
 	OpenVDBModifierData *vdbmd = (OpenVDBModifierData *)md;
 	BLI_addtail(&ob->modifiers, md);
 
+	cache = vdbmd->smoke->domain->point_cache[0];
+
 	BLI_strncpy(vdbmd->filepath, filepath, 1024);
+
+	get_frame_range(ob, vdbmd->filepath, &cache->startframe, &cache->endframe);
+
+	vdbmd->frame_offset = cache->startframe - 1;
+	cache->endframe -= cache->startframe - 1;
+	cache->startframe = 1;
 
 	return OPERATOR_FINISHED;
 }
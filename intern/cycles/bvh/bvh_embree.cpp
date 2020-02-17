d35084b379f0f5337acb15bc8779c5ed2ea441f9
Stefan Werner <stefan@keindesign.de>
2020-02-17 19:47:48 +0100

    Cleanup: code formatting.

--- intern/cycles/bvh/bvh_embree.cpp
+++ intern/cycles/bvh/bvh_embree.cpp
@@ -568,10 +568,14 @@ void BVHEmbree::add_instance(Object *ob, int i)
     for (size_t step = 0; step < num_motion_steps; ++step) {
       RTCQuaternionDecomposition rtc_decomp;
       rtcInitQuaternionDecomposition(&rtc_decomp);
-      rtcQuaternionDecompositionSetQuaternion(&rtc_decomp, decomp[step].x.w, decomp[step].x.x, decomp[step].x.y, decomp[step].x.z);
-      rtcQuaternionDecompositionSetScale(&rtc_decomp, decomp[step].y.w, decomp[step].z.w, decomp[step].w.w);
-      rtcQuaternionDecompositionSetTranslation(&rtc_decomp, decomp[step].y.x, decomp[step].y.y, decomp[step].y.z);
-      rtcQuaternionDecompositionSetSkew(&rtc_decomp, decomp[step].z.x, decomp[step].z.y, decomp[step].w.x);
+      rtcQuaternionDecompositionSetQuaternion(
+          &rtc_decomp, decomp[step].x.w, decomp[step].x.x, decomp[step].x.y, decomp[step].x.z);
+      rtcQuaternionDecompositionSetScale(
+          &rtc_decomp, decomp[step].y.w, decomp[step].z.w, decomp[step].w.w);
+      rtcQuaternionDecompositionSetTranslation(
+          &rtc_decomp, decomp[step].y.x, decomp[step].y.y, decomp[step].y.z);
+      rtcQuaternionDecompositionSetSkew(
+          &rtc_decomp, decomp[step].z.x, decomp[step].z.y, decomp[step].w.x);
       rtcSetGeometryTransformQuaternion(geom_id, step, &rtc_decomp);
     }
   }

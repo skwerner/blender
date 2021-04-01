994bee47b4576d15e53e88b48edaa82b9c2a401a
Sergey Sharybin <sergey@blender.org>
2021-04-01 12:18:15 +0200

    Cycles: Make threading accessors reusable in PathTraceWorkCPU
    Allows to reuse arena creation and kernel globals access between
    various tasks such as path tracing, display buffer update, and
    filtering.
    
    This change also makes it so the display buffer update respects
    the device threads limit.

--- intern/cycles/integrator/path_trace_work_cpu.cpp
+++ intern/cycles/integrator/path_trace_work_cpu.cpp
@@ -27,6 +27,26 @@
 
 CCL_NAMESPACE_BEGIN
 
+/* Create TBB arena for execution of path tracing and rendering tasks. */
+static inline tbb::task_arena local_tbb_arena_create(const Device *device)
+{
+  /* TODO: limit this to number of threads of CPU device, it may be smaller than
+   * the system number of threads when we reduce the number of CPU threads in
+   * CPU + GPU rendering to dedicate some cores to handling the GPU device. */
+  return tbb::task_arena(device->info.cpu_threads);
+}
+
+/* Get CPUKernelThreadGlobals for the current thread. */
+static inline CPUKernelThreadGlobals *kernel_thread_globals_get(
+    vector<CPUKernelThreadGlobals> &kernel_thread_globals)
+{
+  const int thread_index = tbb::this_task_arena::current_thread_index();
+  DCHECK_GE(thread_index, 0);
+  DCHECK_LE(thread_index, kernel_thread_globals.size());
+
+  return &kernel_thread_globals[thread_index];
+}
+
 PathTraceWorkCPU::PathTraceWorkCPU(Device *device,
                                    RenderBuffers *buffers,
                                    bool *cancel_requested_flag)
@@ -52,10 +72,7 @@ void PathTraceWorkCPU::render_samples(int start_sample, int samples_num)
   int offset, stride;
   effective_buffer_params_.get_offset_stride(offset, stride);
 
-  /* TODO: limit this to number of threads of CPU device, it may be smaller than
-   * the system number of threads when we reduce the number of CPU threads in
-   * CPU + GPU rendering to dedicate some cores to handling the GPU device. */
-  tbb::task_arena local_arena(device_->info.cpu_threads);
+  tbb::task_arena local_arena = local_tbb_arena_create(device_);
   local_arena.execute([&]() {
     tbb::parallel_for(int64_t(0), total_pixels_num, [&](int64_t work_index) {
       if (is_cancel_requested()) {
@@ -75,16 +92,14 @@ void PathTraceWorkCPU::render_samples(int start_sample, int samples_num)
       work_tile.offset = offset;
       work_tile.stride = stride;
 
-      const int thread_index = tbb::this_task_arena::current_thread_index();
-      DCHECK_GE(thread_index, 0);
-      DCHECK_LE(thread_index, kernel_thread_globals_.size());
+      CPUKernelThreadGlobals *kernel_globals = kernel_thread_globals_get(kernel_thread_globals_);
 
-      render_samples_full_pipeline(kernel_thread_globals_[thread_index], work_tile, samples_num);
+      render_samples_full_pipeline(kernel_globals, work_tile, samples_num);
     });
   });
 }
 
-void PathTraceWorkCPU::render_samples_full_pipeline(KernelGlobals &kernel_globals,
+void PathTraceWorkCPU::render_samples_full_pipeline(KernelGlobals *kernel_globals,
                                                     const KernelWorkTile &work_tile,
                                                     const int samples_num)
 {
@@ -99,8 +114,8 @@ void PathTraceWorkCPU::render_samples_full_pipeline(KernelGlobals &kernel_global
       break;
     }
 
-    kernels_.integrator_init_from_camera(&kernel_globals, state, &sample_work_tile, render_buffer);
-    kernels_.integrator_megakernel(&kernel_globals, state, render_buffer);
+    kernels_.integrator_init_from_camera(kernel_globals, state, &sample_work_tile, render_buffer);
+    kernels_.integrator_megakernel(kernel_globals, state, render_buffer);
 
     ++sample_work_tile.start_sample;
   }
@@ -120,24 +135,25 @@ void PathTraceWorkCPU::copy_to_gpu_display(GPUDisplay *gpu_display, float sample
     return;
   }
 
-  /* NOTE: This call is supposed to happen outside of any path tracing, so can pick any of the
-   * pre-configured kernel globals. */
-  KernelGlobals *kernel_globals = &kernel_thread_globals_[0];
-
   int offset, stride;
   effective_buffer_params_.get_offset_stride(offset, stride);
 
-  tbb::parallel_for(0, height, [&](int y) {
-    for (int x = 0; x < width; ++x) {
-      kernels_.convert_to_half_float(kernel_globals,
-                                     reinterpret_cast<uchar4 *>(rgba_half),
-                                     reinterpret_cast<float *>(buffers_->buffer.device_pointer),
-                                     sample_scale,
-                                     full_x + x,
-                                     full_y + y,
-                                     offset,
-                                     stride);
-    }
+  tbb::task_arena local_arena = local_tbb_arena_create(device_);
+  local_arena.execute([&]() {
+    tbb::parallel_for(0, height, [&](int y) {
+      for (int x = 0; x < width; ++x) {
+        CPUKernelThreadGlobals *kernel_globals = kernel_thread_globals_get(kernel_thread_globals_);
+
+        kernels_.convert_to_half_float(kernel_globals,
+                                       reinterpret_cast<uchar4 *>(rgba_half),
+                                       reinterpret_cast<float *>(buffers_->buffer.device_pointer),
+                                       sample_scale,
+                                       full_x + x,
+                                       full_y + y,
+                                       offset,
+                                       stride);
+      }
+    });
   });
 
   gpu_display->unmap_texture_buffer();

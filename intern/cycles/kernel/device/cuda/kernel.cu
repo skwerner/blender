22b5c75e4aa4300c8dbe4f325e9bedb1209bb41f
Sergey Sharybin <sergey@blender.org>
2021-04-02 15:21:30 +0200

    Cycles: Remove kernel_adaptive_post_adjust kernel
    It is no longer needed since the scaling is done in buffers.cpp.

--- intern/cycles/kernel/device/cuda/kernel.cu
+++ intern/cycles/kernel/device/cuda/kernel.cu
@@ -290,33 +290,6 @@ extern "C" __global__ void CUDA_LAUNCH_BOUNDS(CUDA_KERNEL_BLOCK_NUM_THREADS,
   }
 }
 
-extern "C" __global__ void CUDA_LAUNCH_BOUNDS(CUDA_KERNEL_BLOCK_NUM_THREADS,
-                                              CUDA_KERNEL_MAX_REGISTERS)
-    kernel_cuda_adaptive_scale_samples(
-        KernelWorkTile *tile, float *render_buffer, int start_sample, int sample, uint work_size)
-{
-  if (kernel_data.film.pass_adaptive_aux_buffer) {
-    int work_index = ccl_global_id(0);
-    bool thread_is_active = work_index < work_size;
-    if (thread_is_active) {
-      uint x = tile->x + work_index % tile->w;
-      uint y = tile->y + work_index / tile->w;
-      int index = tile->offset + x + y * tile->stride;
-      ccl_global float *buffer = render_buffer + index * kernel_data.film.pass_stride;
-      if (buffer[kernel_data.film.pass_sample_count] < 0.0f) {
-        buffer[kernel_data.film.pass_sample_count] = -buffer[kernel_data.film.pass_sample_count];
-        float sample_multiplier = sample / buffer[kernel_data.film.pass_sample_count];
-        if (sample_multiplier != 1.0f) {
-          kernel_adaptive_post_adjust(NULL, buffer, sample_multiplier);
-        }
-      }
-      else {
-        kernel_adaptive_post_adjust(NULL, buffer, sample / (sample - 1.0f));
-      }
-    }
-  }
-}
-
 /* Convert to Display Buffer */
 
 extern "C" __global__ void CUDA_LAUNCH_BOUNDS(CUDA_KERNEL_BLOCK_NUM_THREADS,

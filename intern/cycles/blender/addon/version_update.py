5c8be75426e3708e134750b457d6d72aae01d610
Brecht Van Lommel <brecht@blender.org>
2021-04-02 15:32:04 +0200

    Fix error in Cycles versioning code after removal of branched path

--- intern/cycles/blender/addon/version_update.py
+++ intern/cycles/blender/addon/version_update.py
@@ -182,10 +182,6 @@ def do_versions(self):
             if version <= (2, 79, 0):
                 cscene = scene.cycles
                 # Default changes
-                if not cscene.is_property_set("aa_samples"):
-                    cscene.aa_samples = 4
-                if not cscene.is_property_set("preview_aa_samples"):
-                    cscene.preview_aa_samples = 4
                 if not cscene.is_property_set("blur_glossy"):
                     cscene.blur_glossy = 0.0
                 if not cscene.is_property_set("sample_clamp_indirect"):

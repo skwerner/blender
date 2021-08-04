c4dfe8f9d87537174df8b91439202d117a102171
Brecht Van Lommel <brecht@blender.org>
2021-08-04 20:42:21 +0200

    Cleanup: clang-format

--- intern/cycles/kernel/svm/svm_light_path.h
+++ intern/cycles/kernel/svm/svm_light_path.h
@@ -62,10 +62,9 @@ ccl_device_noinline void svm_node_light_path(INTEGRATOR_STATE_CONST_ARGS,
       /* Read bounce from difference location depending if this is a shadow
        * path. It's a bit dubious to have integrate state details leak into
        * this function but hard to avoid currently. */
-      int bounce = (INTEGRATOR_STATE_IS_NULL) ?
-                       0 :
-                       (path_flag & PATH_RAY_SHADOW) ? INTEGRATOR_STATE(shadow_path, bounce) :
-                                                       INTEGRATOR_STATE(path, bounce);
+      int bounce = (INTEGRATOR_STATE_IS_NULL)    ? 0 :
+                   (path_flag & PATH_RAY_SHADOW) ? INTEGRATOR_STATE(shadow_path, bounce) :
+                                                   INTEGRATOR_STATE(path, bounce);
 
       /* For background, light emission and shadow evaluation we from a
        * surface or volume we are effective one bounce further. */
@@ -80,7 +79,7 @@ ccl_device_noinline void svm_node_light_path(INTEGRATOR_STATE_CONST_ARGS,
     case NODE_LP_ray_transparent: {
       const int bounce = (INTEGRATOR_STATE_IS_NULL) ?
                              0 :
-                             (path_flag & PATH_RAY_SHADOW) ?
+                         (path_flag & PATH_RAY_SHADOW) ?
                              INTEGRATOR_STATE(shadow_path, transparent_bounce) :
                              INTEGRATOR_STATE(path, transparent_bounce);
 

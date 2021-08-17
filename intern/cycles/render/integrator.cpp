skwerner/blender

commit d5019b3838c38742a6f056023604edfa16414675

Fix PMJ pattern automatically enabling adaptive sampling
It should only be the other way around.

intern/cycles/render/integrator.cpp
@@ -181,7 +181,8 @@ void Integrator::device_update(Device *device, DeviceScene *dscene, Scene *scene
                                            FLT_MAX :
                                            sample_clamp_indirect * 3.0f;
 
-  kintegrator->sampling_pattern = sampling_pattern;
+  kintegrator->sampling_pattern = (use_adaptive_sampling) ? SAMPLING_PATTERN_PMJ :
+                                                            sampling_pattern;
 
   if (light_sampling_threshold > 0.0f) {
     kintegrator->light_inv_rr_threshold = 1.0f / light_sampling_threshold;
@@ -198,7 +199,7 @@ void Integrator::device_update(Device *device, DeviceScene *dscene, Scene *scene
   dimensions = min(dimensions, SOBOL_MAX_DIMENSIONS);
 
   if (need_update_lut) {
-    if (sampling_pattern == SAMPLING_PATTERN_SOBOL) {
+    if (kintegrator->sampling_pattern == SAMPLING_PATTERN_SOBOL) {
       uint *directions = dscene->sample_pattern_lut.alloc(SOBOL_BITS * dimensions);
 
       sobol_generate_direction_vectors((uint(*)[SOBOL_BITS])directions, dimensions);
@@ -263,7 +264,7 @@ AdaptiveSampling Integrator::get_adaptive_sampling() const
 {
   AdaptiveSampling adaptive_sampling;
 
-  adaptive_sampling.use = (get_sampling_pattern() == SAMPLING_PATTERN_PMJ);
+  adaptive_sampling.use = use_adaptive_sampling;
 
   if (!adaptive_sampling.use) {
     return adaptive_sampling;

intern/cycles/blender/blender_sync.cpp
@@ -335,15 +335,11 @@ void BlenderSync::sync_integrator(BL::ViewLayer &b_view_layer, bool background)
 
   integrator->set_light_sampling_threshold(get_float(cscene, "light_sampling_threshold"));
 
-  const bool use_adaptive_sampling = RNA_boolean_get(&cscene, "use_adaptive_sampling");
-
   SamplingPattern sampling_pattern = (SamplingPattern)get_enum(
       cscene, "sampling_pattern", SAMPLING_NUM_PATTERNS, SAMPLING_PATTERN_SOBOL);
-  if (use_adaptive_sampling) {
-    sampling_pattern = SAMPLING_PATTERN_PMJ;
-  }
   integrator->set_sampling_pattern(sampling_pattern);
 
+  const bool use_adaptive_sampling = RNA_boolean_get(&cscene, "use_adaptive_sampling");
   integrator->set_use_adaptive_sampling(use_adaptive_sampling);
   integrator->set_adaptive_threshold(get_float(cscene, "adaptive_threshold"));
 

commit 2bf02b14c6f740d16b7b2e1565973cc384e81ce0

Fix wrong viewport render when enabling adaptive sampling in Cycles X
Steps to reproduce:
- Start viewport render
- Enable adaptive sampling

Caused by d5019b3838c: integrator's device update did not properly
detect change in sampler and hence did not rebuild the sampler LUT.

intern/cycles/render/integrator.cpp
@@ -129,16 +129,26 @@ void Integrator::device_update(Device *device, DeviceScene *dscene, Scene *scene
     }
   });
 
+  KernelIntegrator *kintegrator = &dscene->data.integrator;
+
+  /* Adaptive sampling requires PMJ samples.
+   *
+   * This also makes detection of sampling pattern a bit more involved: can not rely on the changed
+   * state of socket, since its value might be different from the effective value used here. So
+   * instead compare with previous value in the KernelIntegrator. Only do it if the device was
+   * updated once (in which case the `sample_pattern_lut` will be allocated to a non-zero size). */
+  const SamplingPattern new_sampling_pattern = (use_adaptive_sampling) ? SAMPLING_PATTERN_PMJ :
+                                                                         sampling_pattern;
+
   const bool need_update_lut = max_bounce_is_modified() || max_transmission_bounce_is_modified() ||
-                               sampling_pattern_is_modified();
+                               dscene->sample_pattern_lut.size() == 0 ||
+                               kintegrator->sampling_pattern != new_sampling_pattern;
 
   if (need_update_lut) {
     dscene->sample_pattern_lut.tag_realloc();
   }
 
   device_free(device, dscene);
-
-  KernelIntegrator *kintegrator = &dscene->data.integrator;
 
   /* integrator parameters */
   kintegrator->min_bounce = min_bounce + 1;
@@ -185,8 +195,7 @@ void Integrator::device_update(Device *device, DeviceScene *dscene, Scene *scene
                                            FLT_MAX :
                                            sample_clamp_indirect * 3.0f;
 
-  kintegrator->sampling_pattern = (use_adaptive_sampling) ? SAMPLING_PATTERN_PMJ :
-                                                            sampling_pattern;
+  kintegrator->sampling_pattern = new_sampling_pattern;
 
   if (light_sampling_threshold > 0.0f) {
     kintegrator->light_inv_rr_threshold = 1.0f / light_sampling_threshold;
fbf5564be16f783c01437f3735502a5efcf3dbb5
Sergey Sharybin <sergey@blender.org>
2021-09-07 11:43:34 +0200

    Revert "Fix possible duplicated tile written on cancel in Cycles X"
    This reverts commit 126c0d1ebd4e5d568dd18971d2b1138965085550.
    
    Committed too quick. Check for the current number of samples for tile
    will not work for the full-frame write. In fact, full-frame is always
    to be written on cancel.
    
    The duplicated tile on cancel could be caused by non-zeroed render
    or display buffer. This needs a deeper investigation.

--- intern/cycles/integrator/render_scheduler.cpp
+++ intern/cycles/integrator/render_scheduler.cpp
@@ -231,14 +231,12 @@ void RenderScheduler::render_work_reschedule_on_cancel(RenderWork &render_work)
 
   /* Do not write tile if it has zero samples it it, treat it similarly to all other tiles which
    * got cancelled. */
-  const bool has_rendered_samples = get_num_rendered_samples() != 0;
-
-  if (!state_.tile_result_was_written && has_rendered_samples) {
+  if (!state_.tile_result_was_written && get_num_rendered_samples() != 0) {
     render_work.tile.write = true;
     render_work.update_display = true;
   }
 
-  if (!state_.full_frame_was_written && has_rendered_samples) {
+  if (!state_.full_frame_was_written) {
     render_work.full.write = true;
     render_work.update_display = true;
   }
